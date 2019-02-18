152aa6cd6845a8de57a5dcb5887ffa6ec4c2573e
FranciscoKen <franciscokenandi@gmail.com>
2019-02-18 16:02:51 +0700

    finished half of code flattening case xml

--- exploration/xml-validator/Node/xml-validator.js
+++ exploration/xml-validator/Node/xml-validator.js
@@ -1,8 +1,10 @@
 var xsd = require('libxml-xsd');
 
-var schemaPath = '../../../standards/alternative.xsd';
-var documentPath = '../../test.xml';
+var schemaPath = "../../../standards/alternative.xsd";
+var documentPath = "../../code-flattening.xml";
+
 xsd.parseFile(schemaPath, function(err, schema){
+  console.log("Err 1:"+err);
   schema.validateFile(documentPath, function(err, validationErrors){
     // err contains any technical error
     // validationError is an array, null if the validation is ok
