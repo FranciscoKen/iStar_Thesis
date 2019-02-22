f14baa2f98f1f8ce02fbe98546edcfb5f96d9f23
FranciscoKen <franciscokenandi@gmail.com>
2019-02-23 01:06:50 +0700

    added 2 false case and 1 true case

--- exploration/xml-validator/Node/xml-validator.js
+++ exploration/xml-validator/Node/xml-validator.js
@@ -1,7 +1,7 @@
 var xsd = require('libxml-xsd');
 
 var schemaPath = "../../../standards/alternative.xsd";
-var documentPath = "../../../testcases/primary-care-nurse-SR.xml";
+var documentPath = "../../../testcases/university-travel-default-istar2.xml";
 
 xsd.parseFile(schemaPath, function(err, schema){
   console.log("Err 1:"+err);
