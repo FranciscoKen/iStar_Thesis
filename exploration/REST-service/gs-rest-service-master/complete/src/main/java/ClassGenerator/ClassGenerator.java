3ab16c9051db19f68fd2e9e85de44ee0bcedc760
franciscoken <franciscokenandi@gmail.com>
2019-03-20 00:25:13 +0700

    experimenting with xml parsing

--- exploration/REST-service/gs-rest-service-master/complete/src/main/java/ClassGenerator/ClassGenerator.java
+++ exploration/REST-service/gs-rest-service-master/complete/src/main/java/ClassGenerator/ClassGenerator.java
@@ -2,30 +2,41 @@ package ClassGenerator;
 
 import net.sourceforge.plantuml.SourceStringReader;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 
 public class ClassGenerator {
-    public static void main(String[] args){
+    private String s_id;
+    private String PATH;
+
+
+    public ClassGenerator(String s_id){
+        this.s_id = s_id;
+        this.PATH = "TEMP/class/";
+    }
+
+    public void generateClassDiagram(){
         OutputStream png = null;
-        try {
-            png = new FileOutputStream("src/main/java/ClassGenerator/test.png");
+        File directory = new File(PATH);
+        if(! directory.exists()){
+            directory.mkdirs();
+        }
+
+        try{
+            png = new FileOutputStream(PATH+s_id+".png");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+
         String source = "@startuml\n";
         source += "Bob -> Alice : hello\n";
         source += "@enduml\n";
 
         SourceStringReader reader = new SourceStringReader(source);
-// Write the first image to "png"
-        try {
+
+        try{
             String desc = reader.outputImage(png).getDescription();
         } catch (IOException e) {
             e.printStackTrace();
         }
-// Return a null string if no generation
     }
 }
