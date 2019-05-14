2c4644415e08275192d5367f7b97113159a7b188
franciscoken <franciscokenandi@gmail.com>
2019-05-14 23:06:15 +0700

    revised some of the error messages

--- product/iStarService/src/main/java/Exception/ExceptionMessages.java
+++ product/iStarService/src/main/java/Exception/ExceptionMessages.java
@@ -4,7 +4,7 @@ public class ExceptionMessages {
     public static final String heading = "ISTAR ERROR : ";
     public static final String sameIDException = "Element ID must be unique";
     public static final String falseSchemaException = "Model doesn't comply to the iStarML XSD";
-    public static final String falseFileTypeException = "Istar Model should be kept in .istarml2 file";
+    public static final String falseFileTypeException = "Istar Model should be saved in .istarml2 file";
     public static final String emptyFileException = "File should not be empty";
     public static final String classResourceNotExistException = "Class Resource does not exist. Please make sure that you are using the right UID or please make another class diagram request";
     public static final String referencedElementNotFoundException ="Element in which you referenced does not exist";
