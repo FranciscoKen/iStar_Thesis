ea503a5b98108904d65fadad3da97f4199d76619
franciscoken <franciscokenandi@gmail.com>
2019-06-07 15:15:28 +0700

    documentation

--- product/iStarService/src/main/java/Exception/IStarException.java
+++ product/iStarService/src/main/java/Exception/IStarException.java
@@ -4,10 +4,7 @@ public class IStarException extends Exception {
     public IStarException(String message) {
         super(message);
     }
-
     public IStarException(String message, Throwable cause) {
         super(message, cause);
     }
-
-
 }
